87adea2864c171fa4d70b6c2f114162720396c23
Jessy Johnson <johnson.jess.ann@gmail.com>
2026-01-27 04:41:36 -0500

    Fixed linting errors.

--- mitigations/session_exp.py
+++ mitigations/session_exp.py
@@ -1,13 +1,11 @@
 from datetime import datetime, timedelta
 
+
 def generate_token_hardened(user_id, username, is_admin):
     return {
         'user_id': user_id,
         'username': username,
         'is_admin': is_admin,
         'iat': datetime.utcnow(),
-        'exp': datetime.utcnow() + timedelta(minutes=15)
+        'exp': datetime.utcnow() + timedelta(minutes=1)
     }
-
-#def verify_token_hardened():
-#    return jwt.decode(token, JWT_SECRET, algorithms=ALGORITHMS, options={"verify_exp": True})
